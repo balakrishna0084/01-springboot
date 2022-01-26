abdc642a9ee9603d86abc4e938870001b0e0e4e1
balakrishnaravuri <cus.balakrish@gmail.com>
2022-01-26 20:03:08 +0530

    Initial commit

---
+++ src/main/java/com/balu/GitspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.balu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitspringbootApplication.class, args);
+	}
+
+}
